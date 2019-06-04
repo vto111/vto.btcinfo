ed90678393912859ef3c0402318c5af9b69fb058
vto111 <vto1111@gmail.com>
2019-06-04 21:08:26 +0300

    correct install module

--- vto.icolor/install/index.php
+++ vto.icolor/install/index.php
@@ -2,12 +2,12 @@
 use \Bitrix\Main\Localization\Loc;
 
 Loc::loadMessages(__FILE__);
-Class Vto_icolor extends CModule
+Class vto_icolor extends CModule
 {
     function __construct()
     {
         $arModuleVersion = array();
-        include (__DIR__."/version.php");
+        include(__DIR__ . "/version.php");
         $this->MODULE_ID = 'vto.icolor';
         $this->MODULE_VERSION = $arModuleVersion["VERSION"];
         $this->MODULE_VERSION_DATE = $arModuleVersion["VERION_DATE"];
@@ -18,30 +18,64 @@ Class Vto_icolor extends CModule
         $this->PARTNER_URI = Loc::getMessage("VTO_ICOLOR_PARTNER_URI");
 
     }
-}
 
-function DoInstall(){
-
-    global $APPLICATION;
-
-    if(true){
-
-    \Bitrix\Main\ModuleManager::registerModule($this->MODULE_ID);
-
-            } else {
-        $APPLICATION->ThrowException(Loc::getMessage("VTO_ICOLOR_ERROR_INSTALL_VERSION"));
+    public function isVersionD7()
+    {
+        return CheckVersion(\Bitrix\Main\ModuleManager::getVersion('main'), '14.00.00');
     }
-}
 
-function DoUninstall(){
+    public function GetPath($notDocumentRoot = false){
+        if ($notDocumentRoot)
+            return str_ireplace(\Bitrix\Main\Application::getDocumentRoot(), '', dirname(__DIR__));
+        else
+            return dirname(__DIR__);
+    }
 
-    if(true){
+    function DoInstall()
+    {
 
-    \Bitrix\Main\ModuleManager::unRegisterModule($this->MODULE_ID);
+        global $APPLICATION;
 
-            } /*else {
-        //$APPLICATION->ThrowException(Loc::getMessage("VTO_ICOLOR_ERROR_INSTALL_VERSION"));
-    }*/
+        if ($this->isVersionD7()) {
+
+            \Bitrix\Main\ModuleManager::registerModule($this->MODULE_ID);
+
+        } else {
+            $APPLICATION->ThrowException(Loc::getMessage("VTO_ICOLOR_ERROR_INSTALL_VERSION"));
+        }
+
+        $APPLICATION->IncludeAdminFile(Loc::getMessage("VTO_ICOLOR_INSTALL_TITLE"), $this->GetPath() . "/install/step.php");
+
+
+    }
+
+    function DoUninstall()
+    {
+
+        global $APPLICATION;
+
+        $context = \Bitrix\Main\Application::getInstance()->getContext();
+        $request = $context->getRequest();
+
+        if ($request["step"] < 2) {
+
+            $APPLICATION->IncludeAdminFile(Loc::getMessage("VTO_ICOLOR_UNINSTALL_TITLE"), $this->GetPath() . "/install/unstep1.php");
+
+        } elseif ($request["step"] == 2) {
+            if ($request["savedata"] != "Y") {
+
+            }
+
+
+            \Bitrix\Main\ModuleManager::unRegisterModule($this->MODULE_ID);
+
+        } else {
+            $APPLICATION->ThrowException(Loc::getMessage("VTO_ICOLOR_ERROR_INSTALL_VERSION"));
+
+        }
+
+    }
+
 }
 
 ?>
--- vto.icolor/install/lang/ru/index.php
+++ vto.icolor/install/lang/ru/index.php
@@ -5,5 +5,7 @@ $MESS ['VTO_ICOLOR_PARTNER_NAME'] = "1C-Bitrix.Pro";
 $MESS ['VTO_ICOLOR_PARTNER_URI'] = "1c-bitrix.pro";
 $MESS ['VTO_ICOLOR_ERROR_INSTALL_VERSION'] = "Error install";
 $MESS ['VTO_ICOLOR_ERROR_UNINSTALL_VERSION'] = "Error uninstall";
+$MESS ['VTO_ICOLOR_INSTALL_TITLE'] = "Title install";
+$MESS ['VTO_ICOLOR_UNINSTALL_TITLE'] = "Title uninstall";
 
 ?>
