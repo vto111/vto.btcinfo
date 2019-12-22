f66225cdbb1db611ce61d252d8ccaeb7d835ed58
vto111 <vto1111@gmail.com>
2019-12-22 03:30:05 +0100

    temp worck

---
+++ vto.icolor/lang/en/index.php
@@ -0,0 +1,9 @@
+<?
+$MESS ['VTO_ICOLOR_MODULE_NAME'] = "IColor";
+$MESS ['VTO_ICOLOR__DESCRIPTION'] = "Подсвечивание просроченных дат в инфоблоке";
+$MESS ['VTO_ICOLOR_PARTNER_NAME'] = "1cb.info";
+$MESS ['VTO_ICOLOR_PARTNER_URI'] = "1cb.info";
+$MESS ['VTO_ICOLOR_ERROR_INSTALL_VERSION'] = "Error install";
+$MESS ['VTO_ICOLOR_ERROR_UNINSTALL_VERSION'] = "Error uninstall";
+
+?>
