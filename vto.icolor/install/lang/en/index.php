c75f5534db674cbf4cde599350909f2b780cbf86
vto111 <vto1111@gmail.com>
2019-12-22 03:49:31 +0100

    remove files

--- vto.icolor/install/lang/en/index.php
+++
@@ -1,9 +0,0 @@
-<?
-$MESS ['VTO_ICOLOR_MODULE_NAME'] = "IColor";
-$MESS ['VTO_ICOLOR__DESCRIPTION'] = "Подсвечивание просроченных дат в инфоблоке";
-$MESS ['VTO_ICOLOR_PARTNER_NAME'] = "1C-Bitrix.Pro";
-$MESS ['VTO_ICOLOR_PARTNER_URI'] = "1c-bitrix.pro";
-$MESS ['VTO_ICOLOR_ERROR_INSTALL_VERSION'] = "Error install";
-$MESS ['VTO_ICOLOR_ERROR_UNINSTALL_VERSION'] = "Error uninstall";
-
-?>
