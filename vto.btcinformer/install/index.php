b262690feaaa86bb8ac6e2ebb0b550c844707aa4
vto111 <vto1111@gmail.com>
2020-01-12 08:18:03 +0100

    create component and copy files

--- vto.btcinformer/install/index.php
+++ vto.btcinformer/install/index.php
@@ -3,7 +3,9 @@ use \Bitrix\Main\Localization\Loc;
 
 Loc::loadMessages(__FILE__);
 Class vto_btcinformer extends CModule
-{
+{   
+    const MODULE_ID = 'vto.btcinformer';
+    
     function __construct()
     {
         $arModuleVersion = array();
@@ -31,21 +33,62 @@ Class vto_btcinformer extends CModule
             return dirname(__DIR__);
     }
     
+    function InstallFiles($arParams = array())
+	{		
+		if (is_dir($p = $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/'.self::MODULE_ID.'/install/components'))
+		{
+			if ($directory = opendir($p))
+			{
+				while (false !== $item = readdir($directory))
+				{
+					if ($item == '..' || $item == '.')
+						continue;
+					CopyDirFiles($p.'/'.$item, $_SERVER['DOCUMENT_ROOT'].'/bitrix/components/'.$item, $ReWrite = True, $Recursive = True);
+				}
+				closedir($directory);
+			}
+		}
+		return true;
+	}
+
+	function UnInstallFiles()
+	{		
+		if (is_dir($p = $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/'.self::MODULE_ID.'/install/components'))
+		{
+			if ($directory = opendir($p))
+			{
+				while (false !== $item = readdir($directory))
+				{
+					if ($item == '..' || $item == '.' || !is_dir($p0 = $p.'/'.$item))
+						continue;
+
+					$directory0 = opendir($p0);
+					while (false !== $item0 = readdir($directory0))
+					{
+						if ($item0 == '..' || $item0 == '.')
+							continue;
+						DeleteDirFilesEx('/bitrix/components/'.$item.'/'.$item0);
+					}
+					closedir($directory0);
+				}
+				closedir($directory);
+			}
+		}
+		return true;
+	}
+    
     function DoInstall()
     {
 
         global $APPLICATION;
 
         if ($this->isVersionD7()) {
-
+            $this->InstallFiles();
             \Bitrix\Main\ModuleManager::registerModule($this->MODULE_ID);
             
         } else {
             $APPLICATION->ThrowException(Loc::getMessage("VTO_BTCINFORMER_ERROR_INSTALL_VERSION"));
         }
-
-        //$APPLICATION->IncludeAdminFile(Loc::getMessage("VTO_BTCINFORMER_INSTALL_TITLE"), $this->GetPath() . "/install/step.php");
-
 
     }
 
@@ -54,17 +97,11 @@ Class vto_btcinformer extends CModule
      */
     function DoUninstall()
     {
-
         global $APPLICATION;
-
-
-            \Bitrix\Main\ModuleManager::unRegisterModule($this->MODULE_ID);
-            //$APPLICATION->IncludeAdminFile(Loc::getMessage("VTO_BTCINFORMER_UNINSTALL_TITLE"), $this->GetPath() . "/install/unstep.php");
-
-
-            
-
-        }
+        \Bitrix\Main\ModuleManager::unRegisterModule($this->MODULE_ID);
+        $this->UnInstallFiles();      
+        
+    }
 
 
 }
