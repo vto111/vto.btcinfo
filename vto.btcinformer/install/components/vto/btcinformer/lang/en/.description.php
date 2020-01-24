b0383983bd406c585ef7f4dd28a54dd3da244ff2
vto111 <vto1111@gmail.com>
2020-01-24 15:50:55 +0100

    add en lang

---
+++ vto.btcinformer/install/components/vto/btcinformer/lang/en/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS["DEVELOPER"] = 'Applications 1cb.info';
+$MESS["COMPONENT_NAME"] = 'Bitcoin widget';
+$MESS["COMPONENT_DESCRIPTION"] = 'The component displays information about the cryptocurrency bitcoin';
+?>
