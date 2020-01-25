0ce6be18dddd372f2fb1991b7067d15da9d100d0
vto111 <vto1111@gmail.com>
2020-01-25 20:54:54 +0100

    convertation conv to cp1251

--- vto.btcinformer/install/components/vto/btcinformer/lang/ru/.parameters.php
+++ vto.btcinformer/install/components/vto/btcinformer/lang/ru/.parameters.php
@@ -1,6 +1,6 @@
 <?
-$MESS["TEMPLATE_FOR_DESCRIPTION_BTC_NAME_BEFORE"] = 'РЎС‚СЂРѕРєР° РѕРїРёСЃР°РЅРёСЏ РґРѕ Р·РЅР°С‡РµРЅРёСЏ';
-$MESS["TEMPLATE_FOR_DESCRIPTION_BTC_NAME_AFTER"] = 'РЎС‚СЂРѕРєР° РѕРїРёСЃР°РЅРёСЏ РїРѕСЃР»Рµ Р·РЅР°С‡РµРЅРёСЏ';
+$MESS["TEMPLATE_FOR_DESCRIPTION_BTC_NAME_BEFORE"] = 'Строка описания до значения';
+$MESS["TEMPLATE_FOR_DESCRIPTION_BTC_NAME_AFTER"] = 'Строка описания после значения';
 $MESS["TEMPLATE_FOR_DESCRIPTION_BTC_NAME_BEFORE_DEFAULT"] = '1 Bitcoin = ';
 $MESS["TEMPLATE_FOR_DESCRIPTION_BTC_NAME_AFTER_DEFAULT"] = '$';
 
--- vto.btcinformer/lang/en/install/index.php
+++ vto.btcinformer/lang/en/install/index.php
@@ -1,16 +1,16 @@
 <?php
 $MESS ['VTO_BTCINFORMER_MODULE_NAME'] = "BTCInformer";
-$MESS ['VTO_BTCINFORMER__DESCRIPTION'] = "Курс BTC";
+$MESS ['VTO_BTCINFORMER__DESCRIPTION'] = "BTC Course";
 $MESS ['VTO_BTCINFORMER_PARTNER_NAME'] = "1cb.info";
 $MESS ['VTO_BTCINFORMER_PARTNER_URI'] = "1cb.info";
 $MESS ['VTO_BTCINFORMER_ERROR_INSTALL_VERSION'] = "Error install";
 $MESS ['VTO_BTCINFORMER_ERROR_UNINSTALL_VERSION'] = "Error uninstall";
 $MESS ['VTO_BTCINFORMER_INSTALL_TITLE'] = "Title install";
-$MESS ['VTO_BTCINFORMER_UNINSTALL_TITLE'] = "Удаление модуля";
-$MESS ['MOD_UNIST_WARN'] = "Вы хотите удалить приложение vto.btcinformer";
-$MESS ['MOD_UNIST_SAVE'] = "Вы можете сохранить таблицы";
-$MESS ['MOD_UNIST_SAVE_TABLES'] = "Сохранить таблицы";
-$MESS ['MOD_UNIST_DEL'] = "Удалить";
+$MESS ['VTO_BTCINFORMER_UNINSTALL_TITLE'] = "Removing the module";
+$MESS ['MOD_UNIST_WARN'] = "Do you want to uninstall the vto.btcinformer application";
+$MESS ['MOD_UNIST_SAVE'] = "You can save tables";
+$MESS ['MOD_UNIST_SAVE_TABLES'] = "Save Tables";
+$MESS ['MOD_UNIST_DEL'] = "Delete";
 $MESS ['VTO_BTCINFORMER_FIELD_TEXT_TITLE'] = "VTO_BTCINFORMER_FIELD_TEXT_TITLE";
 $MESS ['VTO_BTCINFORMER_FIELD_LINE_TITLE'] = "VTO_BTCINFORMER_FIELD_LINE_TITLE";
 
