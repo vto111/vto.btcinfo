cc9a8d4ae9d77ef32c7c6a91ac381ca249976de4
vto111 <vto1111@gmail.com>
2020-01-27 19:18:17 +0100

    correct component

--- vto.btcinformer/install/components/vto/btcinformer/component.php
+++ vto.btcinformer/install/components/vto/btcinformer/component.php
@@ -1,4 +1,8 @@
 <?php
+
+$arCource = array();
+$choosen_source = 0;
+
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 $arSourceDataValue = array(
    "https://blockchain.info/ru/ticker",
@@ -6,15 +10,31 @@ $arSourceDataValue = array(
      
  );
 
-if($arSourceDataValue[$arParams["LINK_SOURCE_DATA"]]){
+if($arParams["LINK_SOURCE_DATA"]){
     $source_data = $arSourceDataValue[$arParams["LINK_SOURCE_DATA"]];
+    $choosen_source = $arParams["LINK_SOURCE_DATA"];
+    
 } else {
     $source_data = $arSourceDataValue[0];
 }
 if ($json = file_get_contents($source_data)) {
         
-            $arResult = json_decode($json, true);
+            $arCource = json_decode($json, true);
         }
+
+if($choosen_source == 0){
+    $arResult["cource"] = $arCource["USD"]["last"];
+} elseif ($choosen_source == 1) {
+    
+    foreach ($arCource as $key=>$cource_coin){
+        if($cource_coin["symbol"] == "bitstampUSD"){
+            $arResult["cource"] = $cource_coin["ask"];
+    
+        }    
+    }
+} else {
+    $arResult["cource"] = $arCource["USD"]["last"];
+}
         
 $arResult['DESCRIPTION_BTC'] = $arParams["TEMPLATE_FOR_DESCRIPTION_BTC"];
 $arResult['DESCRIPTION_USD'] = $arParams["TEMPLATE_FOR_DESCRIPTION_USD"];
--- vto.btcinformer/install/components/vto/btcinformer/templates/.default/template.php
+++ vto.btcinformer/install/components/vto/btcinformer/templates/.default/template.php
@@ -1,24 +1,6 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
 
 <? echo $arResult["DESCRIPTION_BTC"];?>
-
-<?
-
-if($arParams["LINK_SOURCE_DATA"] == 0){
-    echo $arResult["USD"]["last"];
-}
-?>
-
-<? 
-if($arParams["LINK_SOURCE_DATA"] == 1){
-foreach ($arResult as $key=>$cource_coin){
-    if($cource_coin["symbol"] == "bitstampUSD"){
-        echo $arResult[$key]["ask"];
-    }
-    
-}
-}
-?>
-
+<? echo $arResult["cource"];?>
 <? echo $arResult["DESCRIPTION_USD"];?>
 
